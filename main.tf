dec2207ba32aeb33f04fb424ff217d0f5519b804
Tom Yandell <tom.yandell@axetrading.com>
2022-07-20 13:56:50 +0100

    Formatting

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ resource "aws_s3_bucket" "tfstate" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "tfstate" { 
+resource "aws_s3_bucket_public_access_block" "tfstate" {
   bucket = aws_s3_bucket.tfstate.id
 
-  block_public_acls     = true
+  block_public_acls   = true
   block_public_policy = true
 }
 
